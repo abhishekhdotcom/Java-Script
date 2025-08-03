30df5ad1d44096290e0804665bc3e498ccd8f4c5
abhishekh1516techboy <abhishekhkumar1516@gmail.com>
2025-08-03 23:37:57 +0530

    password hashing With Argon2

---
+++ ThapaTechnical_Backend/Tt_v78_Hash_Password_With_Argon2/controllers/auth.controller.js
@@ -0,0 +1,75 @@
+import {
+  createUser,
+  getUserByEmail,
+  hashPassword,
+  comparePassword,
+} from "../services/auth.services.js";
+
+// -------------------Render user Register page-------------------
+export const registerPage = (req, res) => {
+  res.render("userRegister");
+};
+
+// ----------------Register user----------------
+export const registerUser = async (req, res) => {
+  // Get user details from frontend
+  const { name, age, email, password } = req.body;
+
+  // Check user Already exists or not
+  const userExists = await getUserByEmail(email);
+
+  if (userExists) {
+    return res.redirect("/api/v1/auth/register");
+  }
+
+  // Password Hashing
+  const hashedPassword = await hashPassword(password);
+
+  // Create new user in db
+  const [user] = await createUser({
+    name,
+    age,
+    email,
+    password: hashedPassword, // Store hashed password in DB
+  });
+
+  // return API response if user created successfully
+  return res.redirect("/api/v1/auth/login");
+};
+
+// -------------------Render user Login page-------------------
+export const loginPage = (req, res) => {
+  res.render("userLogin");
+};
+
+// ----------------Login user----------------
+export const loginUser = async (req, res) => {
+  // Get user details from frontend (email, password)
+  const { email, password } = req.body;
+
+  // Check user Already exists or not
+  const user = await getUserByEmail(email);
+
+  if (!user) {
+    return res.redirect("/api/v1/auth/login");
+  }
+
+  // Compare Password
+  const isPasswordValid = await comparePassword(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.redirect("/api/v1/auth/login");
+  }
+
+  // set cookies
+  res.cookie("isLoggedIn", true);
+  res.cookie("email", email);
+
+  // send response Logged In Successfully!
+  return res.redirect("/api/v1/auth/profile");
+};
+
+// -------------------Render user Profile page-------------------
+export const profilePage = (req, res) => {
+  res.render("profile", { email: "abhishekhkumar1516@gmail.com", name:"ABhishekh Kumar" });
+};
---
+++ ThapaTechnical_Backend/Tt_v78_Hash_Password_With_Argon2/services/auth.services.js
@@ -0,0 +1,33 @@
+import { db } from "../config/db.js";
+import { eq } from "drizzle-orm";
+import argon2 from "argon2";
+import { usersTable } from "../drizzle/schema.js";
+
+// --------------Get User from DB by Email---------------
+export const getUserByEmail = async (email) => {
+  // Get user from DB
+  const [user] = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+  return user;
+};
+
+// --------------Create User store data in DB---------------
+export const createUser = async ({ name, age, email, password }) => {
+  // Insert data in DB
+  return await db
+    .insert(usersTable)
+    .values({ name, age, email, password })
+    .$returningId();
+};
+
+// ------------------Password hashing & Verify with Argon2------------------
+export const hashPassword = async (password) => {
+  return await argon2.hash(password);
+};
+
+export const comparePassword = async (password, hashedPassword) => {
+  return await argon2.verify(hashedPassword, password);
+};
