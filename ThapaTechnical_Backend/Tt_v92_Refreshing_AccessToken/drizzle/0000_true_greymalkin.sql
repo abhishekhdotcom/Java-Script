a5a47ddff2439cbe735e707f066e71d3cf375f99
abhishekh1516techboy <abhishekhkumar1516@gmail.com>
2025-08-14 00:58:33 +0530

    ReGenerate AccessToken using RefreshToken

---
+++ ThapaTechnical_Backend/Tt_v92_Refreshing_AccessToken/drizzle/0000_true_greymalkin.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `sessions` (
+	`session_id` serial AUTO_INCREMENT NOT NULL,
+	`user_id` int unsigned NOT NULL,
+	`is_valid` tinyint NOT NULL DEFAULT 1,
+	`user_agent` text,
+	`ip` varchar(255),
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `sessions_session_id` PRIMARY KEY(`session_id`)
+);
+--> statement-breakpoint
+CREATE TABLE `users_table` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`age` int NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`created_at` timestamp NOT NULL DEFAULT (now()),
+	`updated_at` timestamp NOT NULL DEFAULT (now()),
+	CONSTRAINT `users_table_id` PRIMARY KEY(`id`),
+	CONSTRAINT `users_table_email_unique` UNIQUE(`email`)
+);
+--> statement-breakpoint
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_users_table_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users_table`(`id`) ON DELETE cascade ON UPDATE no action;
